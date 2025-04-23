82e1d576730c1f1861648278c0efc501f5383c0a
Tormod Liseth <69258630+sifterstudios@users.noreply.github.com>
2025-04-23 14:58:31 +0200

    feat: oil now displays hidden files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,7 +3,18 @@ return {
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+        -- This function defines what is considered a "hidden" file
+        is_hidden_file = function(name, bufnr)
+          local m = name:match("^%.")
+          return m ~= nil
+        end,
+        natural_order = "fast",
+      },
+    },
     -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
